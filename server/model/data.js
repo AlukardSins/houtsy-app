5e79c0417145f7b2cb6fcc21d3a94d0ff80a95e5
Daniel Rodriguez <gameover_lifetime@outlook.com>
2020-06-07 16:24:21 -0500

    Corrected "datetyme" to proper grammar

--- server/model/data.js
+++ server/model/data.js
@@ -3,21 +3,26 @@ const Schema = mongoose.Schema
 
 var Data = new Schema(
   {
-    serviceId:{
-      type: String
-  },
-    userId:{
-      type: String
-  },
-    type:{
-      type: String
-  },
-    datetyme:{
-      type: datetyme
-  },
-    data:{
-      type: String
-  },
+    serviceId: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    dateTime: {
+      type: Date,
+      required: true
+    },
+    data: {
+      type: String,
+      required: true
+    }
   },
   {
     collection: 'data'
