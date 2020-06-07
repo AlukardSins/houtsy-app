712928e56793e13f6d4e5802272b78b09391ec66
JDRoldanL <jesusd.roldan@udea.edu.co>
2020-06-07 12:28:36 -0500

    Creacion de componentes user, auth, data y creacion del share

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { DataComponent } from './components/data/data.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    DataComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
