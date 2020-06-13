b047258c67af9f4b34a0f70926e6ac271c21f089
Daniel Rodriguez <gameover_lifetime@outlook.com>
2020-06-13 12:44:36 -0500

    Change by Zulu

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,22 +1,22 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Injectable } from '@angular/core'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router'
+import { Observable } from 'rxjs'
+import { Router } from '@angular/router'
 import { AuthService } from '../services/auth.service'
 import { BodyComponent } from '../components/home/body/body.component'
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class AuthGuard implements CanActivate{
-      constructor(private auth0: AuthService, private router: Router, private body: BodyComponent){ }
-    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        console.log("logged auth", this.body.logged, this.auth0.loggedIn);
-        
-        if(!this.auth0.loggedIn){
-            this.router.navigateByUrl('/home');
-            return false;
-        }
-        return true;
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor (private auth0: AuthService, private router: Router, private body: BodyComponent) {}
+  canActivate (next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    console.log('logged auth', this.body.logged, this.auth0.loggedIn)
+
+    if (!this.auth0.loggedIn) {
+      this.router.navigateByUrl('/home')
+      return false
     }
+    return true
+  }
 }
