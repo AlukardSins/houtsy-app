dc4847ed1de83f559a462deb3ccb45a568b4519a
JDRoldanL <jesusd.roldan@udea.edu.co>
2020-06-13 17:41:40 -0500

    Lo mismo que el anterior pero para ver si a Zulu le sirve

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core'
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router'
-import { Observable } from 'rxjs'
-import { Router } from '@angular/router'
-import { AuthService } from '../services/auth.service'
-import { BodyComponent } from '../components/home/body/body.component'
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { BodyComponent } from '../components/home/body/body.component';
 
 @Injectable({
   providedIn: 'root'
