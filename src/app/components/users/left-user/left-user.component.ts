3286544857aa2b5afc0758518e1ba5cdd83095c1
JDRoldanL <jesusd.roldan@udea.edu.co>
2020-06-13 22:54:11 -0500

    Creacion del template para la pagina de usuario

---
+++ src/app/components/users/left-user/left-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-user',
+  templateUrl: './left-user.component.html',
+  styleUrls: ['./left-user.component.css']
+})
+export class LeftUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
