cd11ec581ef617d2dbdcd1f31eb74cf2a4c56cb7
Daniel Rodriguez <gameover_lifetime@outlook.com>
2020-06-16 11:09:51 -0500

    Response Mapped on Frontend

--- src/app/components/users/body-user/body-user.component.ts
+++ src/app/components/users/body-user/body-user.component.ts
@@ -1,163 +1,210 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from 'src/app/services/data.service';
-import { interval, Subscription, Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { DataService } from 'src/app/services/data.service'
+import { interval, Subscription, Observable } from 'rxjs'
 
 //Chart
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js'
+import { Color, Label } from 'ng2-charts'
 
 @Component({
   selector: 'app-body-user',
   templateUrl: './body-user.component.html',
-  styleUrls: ['./body-user.component.css']
+  styleUrls: [
+    './body-user.component.css'
+  ]
 })
 export class BodyUserComponent implements OnInit {
-  private userId = localStorage.getItem('userToken');
-  private data: Observable<any> =  this.dataService.getData(this.userId);
-  subscription: Subscription;
+  private userId = localStorage.getItem('userToken')
+  private data: Observable<any> = this.dataService.getData(this.userId)
+  subscription: Subscription
 
-  constructor(private dataService: DataService) { }
+  constructor (private dataService: DataService) {}
 
-  grafica = true;
-  gas = true;
-  energia = true;
-  agua = true;
-  datosAgua = [];
-  datosEnergia = [];
-  datosGas = [];
-  datos = [];
+  grafica = true
+  gas = true
+  energia = true
+  agua = true
+  datosAgua = []
+  datosEnergia = []
+  datosGas = []
+  datos = []
 
-
-  ngOnInit(): void {
-    this.getAllDataSensors();
-    const source = interval(60000);
-    const text = 'Your Text Here';
-    this.subscription = source.subscribe(val => {
-      let userId = localStorage.getItem('userToken');
-      this.dataService.getData(userId).subscribe((res: any) =>{
-        console.log({res}); 
+  ngOnInit (): void {
+    this.getAllDataSensors()
+    const source = interval(60000)
+    const text = 'Your Text Here'
+    this.subscription = source.subscribe((val) => {
+      let userId = localStorage.getItem('userToken')
+      this.dataService.getData(userId).subscribe((res: any) => {
+        console.log({ res })
       })
-    });
+    })
   }
 
   //Abrir y cerrar agua
-  abrirAgua(){
-    this.agua = true;
+  abrirAgua () {
+    this.agua = true
   }
 
-  cerrarAgua(){
-    this.agua = false;
+  cerrarAgua () {
+    this.agua = false
   }
 
   //abrir y cerrar gas
-  abrirGas(){
-    this.gas = true;
+  abrirGas () {
+    this.gas = true
   }
 
-  cerrarGas(){
-    this.gas = false;
+  cerrarGas () {
+    this.gas = false
   }
 
   //abrir y cerrar energia
-  abrirEnergia(){
-    this.energia = true;
+  abrirEnergia () {
+    this.energia = true
   }
 
-  cerrarEnergia(){
-    this.energia = false;
+  cerrarEnergia () {
+    this.energia = false
   }
 
+  getAllDataSensors () {
+    let userId = localStorage.getItem('userToken')
 
-  getAllDataSensors(){
-    let userId = localStorage.getItem('userToken');
-    let water = "Water";
-    let energy = "Energy";
-    let gas = "Gas";
+    this.dataService.getData(userId).subscribe((res: any) => {
+      console.log({ res })
 
-    this.dataService.getData(userId).subscribe((res: any) =>{
-      console.log({res});
-      
-      this.datos.push(res.data.filter(i => water.includes(i.type))); 
-      this.datos.push(res.data.filter(i => energy.includes(i.type))); 
-      this.datos.push(res.data.filter(i => gas.includes(i.type))); 
-      this.datosAgua.push(this.datos[0].data);
-      this.datosEnergia.push(this.datos[1].data);
-      this.datosGas.push(this.datos[2].data);
+      res.data.map((dataSet) => {
+        if (dataSet.type === 'Water') {
+          this.datosAgua.push(dataSet.data)
+        } else if (dataSet.type === 'Energy') {
+          this.datosEnergia.push(dataSet.data)
+        } else if (dataSet.type === 'Gas') {
+          this.datosGas.push(dataSet.data)
+        }
+      })
+      console.log('Chart data: ', this.lineChartData[0])
+    })
+    console.log(this.datos)
 
-      this.lineChartData[0].data.push(this.datosAgua)
-      this.lineChartData[1].data.push(this.datosEnergia);
-      this.lineChartData[2].data.push(this.datosGas);
-      console.log("Chart data: ", this.lineChartData[0]);
-      
-    });
-    console.log(this.datos);
-    
-    console.log("Agua", this.datosAgua);
-    console.log("Energia", this.datosEnergia);
-    console.log("Gas", this.datosGas);
-    
+    console.log('Agua', this.datosAgua)
+    console.log('Energia', this.datosEnergia)
+    console.log('Gas', this.datosGas)
   }
 
-
-  ngOnDestroy(): void{
-
-
-  }
-
-
+  ngOnDestroy (): void {}
 
   //chart
   lineChartData: ChartDataSets[] = [
     { data: this.datosAgua, label: 'Agua' },
     { data: this.datosEnergia, label: 'Energia' },
     { data: this.datosGas, label: 'Gas' }
-  ];
+  ]
 
   //Labels shown on the x-axis
-  lineChartLabels: Label[] = ['0','1','2','3','4','5','6','7','8','9','9','10','11','12','13','14','15','16','17','18','19','20','21','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59'];
-  
-  
+  lineChartLabels: Label[] = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '21',
+    '22',
+    '23',
+    '24',
+    '25',
+    '26',
+    '27',
+    '28',
+    '29',
+    '30',
+    '31',
+    '32',
+    '33',
+    '34',
+    '35',
+    '36',
+    '37',
+    '38',
+    '39',
+    '40',
+    '41',
+    '42',
+    '43',
+    '44',
+    '45',
+    '46',
+    '47',
+    '48',
+    '49',
+    '50',
+    '51',
+    '52',
+    '53',
+    '54',
+    '55',
+    '56',
+    '57',
+    '58',
+    '59'
+  ]
+
   // Define chart options
   lineChartOptions: ChartOptions = {
     responsive: true
-  };
+  }
 
   // Define colors of chart segments
   lineChartColors: Color[] = [
-
     //Fisrt chart - Agua
     {
       backgroundColor: 'rgba(34, 49, 63, 1)',
-      borderColor: 'rgba(37, 116, 169, 1)',
+      borderColor: 'rgba(37, 116, 169, 1)'
     },
     //Second chart - Energy
     {
       backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
+      borderColor: 'red'
     },
     //Third chart - Gas
-    { 
+    {
       backgroundColor: 'rgba(38, 166, 91, 1)',
-      borderColor: 'rgba(0, 230, 64, 1)',
-    },
-
-  ];
+      borderColor: 'rgba(0, 230, 64, 1)'
+    }
+  ]
 
   // Set true to show legends
-  lineChartLegend = true;
+  lineChartLegend = true
 
   // Define type of chart
-  lineChartType = 'line';
+  lineChartType = 'line'
 
-  lineChartPlugins = [];
+  lineChartPlugins = []
 
   // events
-  chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
+  chartClicked ({ event, active }: { event: MouseEvent; active: {}[] }): void {
+    console.log(event, active)
   }
 
-  chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
+  chartHovered ({ event, active }: { event: MouseEvent; active: {}[] }): void {
+    console.log(event, active)
   }
-
 }
